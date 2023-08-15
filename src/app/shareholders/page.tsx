ee127f07382a8083356a3acae54e8cae8b667d4c
Flash7227 <punk_lzi23@yahoo.com>
2023-08-15 21:48:32 +0800

    sharehodlers main page fetch fixed

--- src/app/shareholders/page.tsx
+++ src/app/shareholders/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
 import { getShareHolders } from "@/api/rest";
 import Breadcrumb from "@/components/ui/breadcrumb";
 import { format_date } from "@/lib/helper";
 
 const breadcrumb = ["Хувьцаа эзэмшигчдэд"];
-const Page = async () => {
-  const news = await getShareHolders();
+const Page = () => {
+  const [news, setNews] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getShareHolders();
+      setNews(res);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Breadcrumb data={breadcrumb} />
@@ -12,13 +22,13 @@ const Page = async () => {
         {news && (
           <div>
             <div className="text-center my-4 text-2xl font-semibold text-brand-1 border-b border-brand-1/20 pb-4 tracking-tight">
-              {news["data"][0].title}
+              {news["data"][0]['title']}
             </div>
-            <div className="news">
-              <div dangerouslySetInnerHTML={{ __html: news["data"][0].body }} />
+            <div className="news" suppressHydrationWarning>
+              <div dangerouslySetInnerHTML={{ __html: news["data"][0]['body'] }} />
             </div>
             <div className="text-right font-bold text-sm text-brand-1/80 my-2 uppercase">
-              Нийтлэгдсэн {format_date(news["data"][0].created_at)}
+              Нийтлэгдсэн {format_date(news["data"][0]['created_at'])}
             </div>
           </div>
         )}
