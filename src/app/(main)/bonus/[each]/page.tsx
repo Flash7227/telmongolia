9d8a28377293da3f80f98a0200efb9ce384afff3
Flash7227 <punk_lzi23@yahoo.com>
2023-08-15 19:14:22 +0800

    fetch test

--- src/app/(main)/bonus/[each]/page.tsx
+++ src/app/(main)/bonus/[each]/page.tsx
@@ -1,37 +1,49 @@
 "use client"
+import { useEffect, useState } from "react";
 import { getEachNews } from "@/api/rest";
 import { format_date } from "@/lib/helper";
 import Breadcrumb from "@/components/ui/breadcrumb";
 import parse from 'html-react-parser';
 
+async function getData() {
+  try {
+    const res = await fetch('https://api2.telecommongolia.mn/v1/news?id=16');
+    // const url = process.env.API2 + '/' + 'news' + '?id=' + params.each;
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 const breadcrumb = ["Урамшуулал"];
-const Page = async ({ params }: { params: { each: number } }) => {
-  // const news = await getEachNews(params.each, "news");
-  const getNews = async ()=> {
-    const url = process.env.API2 + '/' + 'news' + '?id=' + params.each;
-    try {
-      const response = await fetch(url);
-      let result = await response.json();
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  const news = await getNews();
+const Page = ({ params }: { params: { each: number } }) => {
+  const [news, setNews] = useState();
+    useEffect(() => {
+      const fetchData = async () => {
+        const posts = await getData();
+        setNews(posts);
+      };
+      const news = fetchData();
+    }, []);
+
   return (
     <div className="mt-5">
       <Breadcrumb data={breadcrumb} />
       {news && (
         <div>
           <div className="text-center my-4 text-2xl font-semibold text-brand-1 border-b border-brand-1/20 pb-4 tracking-tight">
-            {news.title}
+            {news['title']}
           </div>
           <div className="news" suppressHydrationWarning>
-            {parse(news.body)}
+            {parse(news['body'])}
           </div>
           <div className="text-right font-bold text-sm text-brand-1/80 my-2 uppercase">
-            Нийтлэгдсэн {format_date(news.created_at)}
+            Нийтлэгдсэн {format_date(news['created_at'])}
           </div>
         </div>
       )}
