8a8f128df21db500985506b5881a7ef62f61e308
Flash7227 <punk_lzi23@yahoo.com>
2024-05-07 16:33:11 +0800

    audit finance 2023 added

--- src/app/company/public/audited/page.tsx
+++ src/app/company/public/audited/page.tsx
@@ -3,9 +3,11 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Аудитлагдсан тайлан"];
 const duties = [
+    ['2023 оны санхүүгийн аудитын тайлан', 'iltodbaidal/sanhuuaudit2023.pdf'],
     ['2022 оны санхүүгийн тайланд хийсэн аудитын тайлан', 'iltodbaidal/audit2022.pdf'],
     ['2018 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2018.pdf'],
     ['2017 оны санхүүгийн тайлан өгсөн хөндлөнгийн аудитын дүгнэлт', 'iltodbaidal/audit2017.pdf']
+    
 ];
 const Page = () => {
     return (
