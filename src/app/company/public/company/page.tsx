9df130262cb64364845d47b39283023e02c30a99
Flash7227 <punk_lzi23@yahoo.com>
2024-10-29 10:36:40 +0800

    pdf added

--- src/app/company/public/company/page.tsx
+++ src/app/company/public/company/page.tsx
@@ -3,7 +3,8 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Компанийн засаглалын тайлан"];
 const duties = [
-    ['Компанийн засаглалын тайлан 20240515', 'iltodbaidal/MTC2023-КЗ_ТАЙЛАН.PDF']    
+    ['Компанийн засаглалын тайлан 20240515', 'iltodbaidal/MTC2023-КЗ_ТАЙЛАН.PDF'],
+    ['ЭБАТ-ны цалин хөлс', 'iltodbaidal/ЭБАТ-ны цалин хөлс.pdf']    
 ];
 const Page = () => {
     return (
