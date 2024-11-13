b218969cf92e68c4c7450ecf2baaf5b423817a3b
Flash7227 <punk_lzi23@yahoo.com>
2024-11-13 10:08:28 +0800

    tailans added

--- src/app/company/public/financial/page.tsx
+++ src/app/company/public/financial/page.tsx
@@ -3,6 +3,7 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Санхүүгийн тайлан"];
 const duties = [
+    ['2024 оны 2-р улирлын тайлан', '/iltodbaidal/2024uliral.pdf'],
     ['2023 оны 4-р улирлын тайлан', '/iltodbaidal/20920234report.pdf'],
     ['2022 оны 2-р улирлын тайлан', 'https://mse.mn/uploads/finance/20920222report.pdf'],
     ['2021 оны 4-р улирлын тайлан', 'https://mse.mn/uploads/finance/20920214report.pdf'],
--- src/app/company/public/seasonly/page.tsx
+++ src/app/company/public/seasonly/page.tsx
@@ -3,6 +3,7 @@ import PdfTable from "../../pdfTable";
 
 const breadcrumb = ["Ил тод байдал", "Улирлын тайлан"];
 const duties = [
+    ['Хагас жилийн тайлан 2024', 'iltodbaidal/2024_semi_annual_report.pdf'],
     ['Хагас жилийн тайлан 2020', 'iltodbaidal/2020hagasjil.pdf'],
     ['Хагас жилийн тайлан 2019', 'iltodbaidal/2019hagasjil.pdf'],
     ['Хагас жилийн тайлан 2018', 'iltodbaidal/2018hagasjil.pdf']
