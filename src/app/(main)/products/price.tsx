a62b05b4fa286927c76a1731ae8ba56fccbf6091
miigaa0204 <mytawochir@gmail.com>
2024-11-29 16:55:24 +0800

    new change bundle

--- src/app/(main)/products/price.tsx
+++ src/app/(main)/products/price.tsx
@@ -1,4 +1,4 @@
-const Price = ({title, price, list}:{title:string, price:any, list:string[]}) => {
+const Price = ({title, price, list, alert}:{title:string, price:any, list:string[], alert?:any}) => {
     return (
         <div className="w-[260px] border p-4 rounded-xl text-center bg-brand-1/10 hover:shadow-md hover:shadow-brand-2/60 cursor-pointer transition-shadow">
             <div>
@@ -6,6 +6,7 @@ const Price = ({title, price, list}:{title:string, price:any, list:string[]}) =>
                 <div className="w-[130px] h-[130px] mx-auto flex items-center justify-center rounded-full bg-white"></div>
                 <h5 className="text-3xl font-semibold mt-4 text-brand-3 uppercase">{title}</h5>
                 <h4  className="text-2xl font-bold mt-1 text-brand-2">{price}</h4>
+                {alert && <div className="text-red-700 text-sm">{alert}</div>}
                 <ul className="my-6 text-[14px] flex flex-col gap-6 tracking-tight leading-5 text-slate-900">
                     {
                         list.map((d, index:number)=>(
